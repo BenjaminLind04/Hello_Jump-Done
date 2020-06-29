2d3323fdcb83d32c567c8b101a37cf023bb22412
Benjamin L <benjamin.lindsten@gmail.com>
2020-06-29 13:29:35 +0200

    Updated game

---
+++ object/factory.go
@@ -0,0 +1,51 @@
+components {
+  id: "factory"
+  component: "/objekt/factory.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "platform"
+  type: "factory"
+  data: "prototype: \"/objekt/platform.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "jumper"
+  type: "factory"
+  data: "prototype: \"/objekt/jumper.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
